fd4f618f72ef308a376c631fc5ea7e4a3b1a055f
world-pc <287334486+world-pc@users.noreply.github.com>
2026-06-01 08:09:01 -0400

    finished the botMoveState fn in bot.js. has yet to be tested.

--- bot.js
+++ bot.js
@@ -1,11 +1,17 @@
-function botMove(ball_x, ball_y, ball_vx, ball_va, bot_orientationy) {
+function botMoveState(ball_x, ball_y, ball_vx, ball_va,
+                 paddle_ypos, paddle_xpos) {
     //returns the appropriate paddle state given ball position / velocity
     //and which side the bot is on.
     
-    //if bot is on left side of screen
-    if(bot_orientation < 0) {
-    }
-    else {
+    //we can make this conditional neater via math later.
+    if((paddle_xpos < 0 && ball_vx < 0)||
+       (paddle_xpos > 0 && ball_vx > 0)) {
+
+       if(ball_y > paddle_ypos) {
+           return 'up';
+       } else if(ball_y < paddle_ypos) {
+           return 'down';
     }
 
+    return 'neutral';
 }
