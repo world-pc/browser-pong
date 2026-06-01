cef5d7584776616fd4f70213c1be859273b1d024
world-pc <287334486+world-pc@users.noreply.github.com>
2026-06-01 07:30:30 -0400

    created pong bot js file (bot.js)

---
+++ bot.js
@@ -0,0 +1,11 @@
+function botMove(ball_x, ball_y, ball_vx, ball_va, bot_orientationy) {
+    //returns the appropriate paddle state given ball position / velocity
+    //and which side the bot is on.
+    
+    //if bot is on left side of screen
+    if(bot_orientation < 0) {
+    }
+    else {
+    }
+
+}
