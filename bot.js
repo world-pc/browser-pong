68c1a050281d024acc6b6ba6c7c91a5992eba2c3
world-pc <287334486+world-pc@users.noreply.github.com>
2026-06-02 08:30:11 -0400

    made bot.js a module exporting the botMoveState function.

--- bot.js
+++ bot.js
@@ -1,17 +1,19 @@
-function botMoveState(ball_x, ball_y, ball_vx, ball_va,
-                 paddle_ypos, paddle_xpos) {
-    //returns the appropriate paddle state given ball position / velocity
-    //and which side the bot is on.
-    
-    //we can make this conditional neater via math later.
-    if((paddle_xpos < 0 && ball_vx < 0)||
-       (paddle_xpos > 0 && ball_vx > 0)) {
+module.exports = {
+    botMoveState: function (ball_x, ball_y, ball_vx, ball_va,
+                     paddle_ypos, paddle_xpos) {
+        //returns the appropriate paddle state given ball position / velocity
+        //and which side the bot is on.
+        
+        //we can make this conditional neater via math later.
+        if((paddle_xpos < 0 && ball_vx < 0)||
+           (paddle_xpos > 0 && ball_vx > 0)) {
 
-       if(ball_y > paddle_ypos) {
-           return 'up';
-       } else if(ball_y < paddle_ypos) {
-           return 'down';
+           if(ball_y > paddle_ypos) {
+               return 'up';
+           } else if(ball_y < paddle_ypos) {
+               return 'down';
+        }
+
+        return 'neutral';
     }
-
-    return 'neutral';
-}
+};
